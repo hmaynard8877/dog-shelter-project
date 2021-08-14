318cf2ecd26750a1a4fe78706cf68996a0199711
hmaynard8877 <hmaynard8877@gmail.com>
2021-08-14 14:57:26 -0700

    Start defining function and creating unit tests

--- food_calculator.py
+++ food_calculator.py
@@ -1,2 +1,15 @@
+MAX_CAPACITY = 30
+
 def calculate_food(num_small, num_medium, num_large, lbs_surplus):
-    print("Hello, world!")
+    if (num_small + num_medium + num_large > MAX_CAPACITY):
+        raise Exception("Number of dogs exceeds capacity.")
+    if (type(num_small) != int or type(num_medium) != int or type(num_large) != int):
+        raise TypeError("Number of dogs must be an integer.")
+    if (num_small < 0 or num_medium < 0 or num_large < 0 or lbs_surplus < 0):
+        raise Exception("Values entered must be positive.")
+
+    food_amount = (((num_small * 10) + (num_medium * 20) + (num_large * 30)) - lbs_surplus) * 1.2
+    if (food_amount < 0):
+        return 0
+    else:
+        return round(food_amount, 2)
--- test_food_calculator.py
+++ test_food_calculator.py
@@ -1,2 +1,28 @@
 import unittest
 import food_calculator
+
+class TestFoodCalculator(unittest.TestCase):
+    def test_food_calc(self):
+        self.assertEqual(food_calculator.calculate_food(5, 3, 7, 17), 363.6)
+        self.assertEqual(food_calculator.calculate_food(0, 0, 0, 14), 0)
+        self.assertEqual(food_calculator.calculate_food(0, 0, 30, 0), 1080)
+        self.assertEqual(food_calculator.calculate_food(3, 2, 1, 100), 0)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(10, 10, 11, 10)
+
+        with self.assertRaises(TypeError):
+            food_calculator.calculate_food(5.4, 4, 6, 5)
+        
+        with self.assertRaises(TypeError):
+            food_calculator.calculate_food(5, "cat", 6, 5)
+
+        with self.assertRaises(TypeError):
+            food_calculator.calculate_food(5, 4, 6.8, 5)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(0, -4, 6, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
