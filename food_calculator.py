432b00ba346b2050af7bb91b341dd19bc41b7e1a
hmaynard8877 <hmaynard8877@gmail.com>
2021-08-15 13:22:54 -0700

    Additional unit tests and comments

--- food_calculator.py
+++ food_calculator.py
@@ -1,13 +1,20 @@
 MAX_CAPACITY = 30
 
 def calculate_food(num_small, num_medium, num_large, lbs_surplus):
-    if (num_small + num_medium + num_large > MAX_CAPACITY):
-        raise Exception("Number of dogs exceeds capacity.")
+    #Check that number of dog values are integers
     if (type(num_small) != int or type(num_medium) != int or type(num_large) != int):
-        raise TypeError("Number of dogs must be an integer.")
+        raise TypeError("Error: Number of dogs must be an integer.")
+    #Check that all values are positive
     if (num_small < 0 or num_medium < 0 or num_large < 0 or lbs_surplus < 0):
-        raise Exception("Values entered must be positive.")
+        raise Exception("Error: Values entered must be positive.")
+    #Check that values are not null
+    if (num_small is None or num_medium is None or num_large is None or lbs_surplus is None):
+        raise Exception("Error: At least one variable is set to None.")
+    #Check number of dogs against maximum capacity of shelter
+    if (num_small + num_medium + num_large > MAX_CAPACITY):
+        raise Exception("Error: Number of dogs exceeds capacity.")
 
+    #Calculate amount of dog food to order for next month
     food_amount = (((num_small * 10) + (num_medium * 20) + (num_large * 30)) - lbs_surplus) * 1.2
     if (food_amount < 0):
         return 0
--- test_food_calculator.py
+++ test_food_calculator.py
@@ -3,15 +3,25 @@ import food_calculator
 
 class TestFoodCalculator(unittest.TestCase):
     def test_food_calc(self):
+        #Verify example from assignment instructions
         self.assertEqual(food_calculator.calculate_food(5, 3, 7, 17), 363.6)
+        #Verify no dogs in shelter
         self.assertEqual(food_calculator.calculate_food(0, 0, 0, 14), 0)
+        #Verify no leftover food
+        self.assertEqual(food_calculator.calculate_food(5, 3, 7, 0), 384)
+        #Verify maximum amount of food needed for shelter
         self.assertEqual(food_calculator.calculate_food(0, 0, 30, 0), 1080)
+        #Verify enough leftover food to cover next month
         self.assertEqual(food_calculator.calculate_food(3, 2, 1, 100), 0)
+        self.assertEqual(food_calculator.calculate_food(3, 2, 1, 220), 0)
+        #Verify float value for extra food
         self.assertEqual(food_calculator.calculate_food(3, 2, 1, 20.5), 95.4)
 
+        #Verify too many dogs in the shelter raises exception
         with self.assertRaises(Exception):
             food_calculator.calculate_food(10, 10, 11, 10)
 
+        #Verify invalid entry for number of dogs raises exception
         with self.assertRaises(TypeError):
             food_calculator.calculate_food(5.4, 4, 6, 5)
         
@@ -19,10 +29,33 @@ class TestFoodCalculator(unittest.TestCase):
             food_calculator.calculate_food(5, "cat", 6, 5)
 
         with self.assertRaises(TypeError):
-            food_calculator.calculate_food(5, 4, 6.8, 5)
+            food_calculator.calculate_food(5, 4, True, 5)
+
+        #Verify negitive values raises exception
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(-2, 4, 6, 5)
 
         with self.assertRaises(Exception):
-            food_calculator.calculate_food(0, -4, 6, 5)
+            food_calculator.calculate_food(2, -4, 6, 5)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(2, 4, -6, 5)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(2, 4, 6, -5)
+
+        #Verify None values raises exception
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(None, 3, 5, 10)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(3, None, 5, 10)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(3, 3, None, 10)
+
+        with self.assertRaises(Exception):
+            food_calculator.calculate_food(3, 3, 5, None)
 
 
 if __name__ == '__main__':
